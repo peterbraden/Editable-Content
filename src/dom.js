ab7b853797e34cb64d5790eba1dd570154a431a6
Peter Braden <peterbraden@peterbraden.co.uk>
2012-02-23 18:10:08 -0800

    debug

--- src/dom.js
+++ src/dom.js
@@ -259,7 +259,7 @@ r.prototype._initFromIndices = function(elem, start, end){
   
     // IE7 Bug
     if (this.raw.text.length != end-start)  
-      this.raw = (end - start) - this.raw.text.length
+      this.raw = {toString: function(){return (end - start) - this.raw.text.length}}
   }
     
   
